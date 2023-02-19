3051078c8b2ada4d4d99335a6efbe5176e8045b9
Kyriece Dam s3848845 <s3848845@student.rmit.edu.au>
2023-02-19 16:08:13 +1100

    Face fading in centered

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const toggle = document.querySelector("#dark-mode-toggle");
-    const body = document.querySelector("body");
+  const toggle = document.querySelector("#dark-mode-toggle");
+  const body = document.querySelector("body");
   
-    toggle.addEventListener("change", function() {
-      if (toggle.checked) {
-        body.style.backgroundColor = "#333";
-        body.style.color = "#fff";
-      } else {
-        body.style.backgroundColor = "#fff";
-        body.style.color = "#000";
-      }
-    });
+  toggle.addEventListener("change", function() {
+    if (toggle.checked) {
+      body.style.backgroundColor = "#333";
+      body.style.color = "#fff";
+    } else {
+      body.style.backgroundColor = "#fff";
+      body.style.color = "#000";
+    }
   });
+});
